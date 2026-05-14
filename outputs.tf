77359a722b8266f5312d9400e6d506b50cbb1d23
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-05-14 20:51:50 +0200

    fix outputs descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 # outputs.tf
 
 output "client_id" {
-  description = "Client ID for the Kubernetes operator"
+  description = "Client ID for the machine identity"
   value       = infisical_identity_universal_auth_client_secret.this.client_id
   sensitive   = true
 }
 
 output "client_secret" {
-  description = "Client secret for the Kubernetes operator"
+  description = "Client secret for the machine identity"
   value       = infisical_identity_universal_auth_client_secret.this.client_secret
   sensitive   = true
 }
 
 output "environment_slug" {
-  description = "Environment slug to create folders"
+  description = "Environment slug used to create folders"
   value       = var.environment_slug
 }
 
